ba21fc1f51d3cfe36dad9007be9ee965491e6742
Greg <greg.graf@mtvn.com>
2011-12-21 17:10:27 -0500

    trying to do the http stuff

--- tests/comparator.js
+++ tests/comparator.js
@@ -1,3 +1,44 @@
+	var http = require('http');
+
+http.createServer(function (req, res) {
+
+}).listen(8081, "0.0.0.0");
+
+
+var fileCacheWriter = function() {
+
+	
+	var origin = "localhost";
+	
+	var site = http.createClient(9200, origin);
+	
+	var write = function(item) {
+
+		var request = site.request("GET", item.url);
+		request.end();
+
+		console.log("requesting")
+
+		item.waiting.forEach(function(ele, ind, arr) {
+		console.log("assinging")
+			request.on('response', function (response) {
+		console.log("responding")
+				response.pipe(ele.res);
+				
+			});	
+		})
+
+		
+
+	
+	}
+
+	return {
+		"write":write
+	}
+
+}()
+
 
 var refreshManager = function () {
 
@@ -8,7 +49,7 @@ var refreshManager = function () {
 	
 	var processing = 0;
 
-	var add = function(url) {
+	var add = function(url, res) {
 		var pos;
 		
 		if (_map[url]) {
@@ -20,6 +61,7 @@ var refreshManager = function () {
 			_map[url] = {
 				"url": url,
 				"count": 0,
+				"waiting": []
 			}
 	
 			pos = _queue.push(_map[url]);
@@ -27,10 +69,21 @@ var refreshManager = function () {
 			_map[url].queueEntry = _queue[pos-1];
 		}
 		
+		if (res) 
+			_map[url].waiting.push(res);
+		
+		return _map[url]
+		
 	}
 	
 	var compare = function(item1, item2) {
-
+	
+		if (item1.waiting.length > item2.waiting.length)
+			return -1
+			
+		if (item2.waiting.length > item1.waiting.length)
+			return 1
+			
 		if (item1.count > item2.count)
 			return -1
 			
@@ -50,23 +103,34 @@ var refreshManager = function () {
 		processing++;
 		
 		_queue.sort(compare);
-		console.log(_queue[0])
 		
 		_next = _queue.shift();
-		console.log(_next.url)
-		console.log(_next.queueEntry)
+
 		delete _next.queueEntry
-		console.log(_next.queueEntry)
+
 		return _next
+	}
+
+	var checkQueue = function() {
+	
+		console.log("checkQueue")
+		console.log(next())
+		var item = next();
+
+		if (item) {
+			console.log("item")
+			fileCacheWriter.write(item)
+		}
 	}
 
 	return {
 		"add": add,
-		"next": next
+		"next": next,
+		"checkQueue": checkQueue,
+		"_queue": _queue
 	}
 
 }()
-
 
 
 
@@ -77,23 +141,47 @@ var assert = require("assert");
 
 var test;
 var urls = ["url1", "url2", "url3", "url2", "url4", "url2", "url5", "url1", "url2", "url6"];
+var writerUrls = [0,0,0,0,0,0,1,0,0,0]
+var sorted = ["url5", "url2", "url1", "url3", "url4", "url6"]
 
-var sorted = ["url2", "url1", "url3", "url4", "url5", "url6"]
 
 urls.forEach(function(ele, ind, arr) {
-	requestQueue.add(ele);
+	refreshManager.add(ele, writerUrls[ind]);
 })
 
+/** test sorting function **/
 
-while (test = requestQueue.next().url) {
+while (test = refreshManager.next().url) {
 	val = sorted.shift();
 	console.log(test + ", " + val)
 	assert.equal(test, val, "Should be " + val + ", was " + test)
 }
 
 
+/** test add function **/
+
+	test = refreshManager.add("url1").waiting.length;
+	val = 0;
+	console.log(test + ", " + val)
+	
+	assert.equal(test, val, "Should be " + val + ", was " + test)
+
+	test = refreshManager.add("url1", true).waiting.length;
+	val = 1;
+	console.log(test + ", " + val)
+	assert.equal(test, val, "Should be " + val + ", was " + test)
+
+
+
 /*
 	test that when we get next, we break the links between the _map and the queue
 
 */
+
+urls.forEach(function(ele, ind, arr) {
+	console.log("add")
+	refreshManager.add(ele, writerUrls[ind]);
+})
+console.log(refreshManager._queue)
+//refreshManager.checkQueue();
 
